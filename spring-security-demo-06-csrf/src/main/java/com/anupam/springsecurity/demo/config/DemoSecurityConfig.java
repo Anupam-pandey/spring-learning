be37bd0eee1229f14cdd87bb714e89a86c468390
Anupam-pandey <pandey.anupam92@gmail.com>
2023-02-10 01:05:49 +0530

    q
    f

---
+++ spring-security-demo-06-csrf/src/main/java/com/anupam/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.anupam.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@EnableWebSecurity
+@Configuration
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().
+		loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
+		.and().logout().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("anu").password("123").roles("admin"))
+		.withUser(users.username("pam").password("123").roles("mgr"))
+		.withUser(users.username("pand").password("123").roles("emp"));
+		
+	}
+
+}
---
+++ spring-security-demo-06-csrf/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,17 @@
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+welcome!!
+<form:form action="${pageContext.request.contextPath}/logout" method="POST">
+<input type="submit" value="logout">
+</form:form>
+</body>
+</html>
